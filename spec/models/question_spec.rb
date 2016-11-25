38d5a245bf4e55c6613adaa8163d11bfa698b660
savvinovan <savvinovan@gmail.com>
2016-11-25 14:57:58 +0900

    FIXED question model rspec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it 'should create new Quesiton' do
-    Question.create!(name: '123123', text: 'asdfasdf asdfasdf')
+    Question.create!(name: 123123, text: 'asdfasdf asdfasdf')
 
-    expect(Question.last.name).to eq('123123')
+    expect(Question.last.name).to eq(123123)
     expect(Question.last.text).to eq('asdfasdf asdfasdf')
   end
 end
